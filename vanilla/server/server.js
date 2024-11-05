f0e9882821e4c9b311874e6bf94a587311f393ca
Shariq <shariqyousuf01@gmail.com>
2024-11-05 12:10:00 +0500

    update at 5 nov in class

--- vanilla/server/server.js
+++ vanilla/server/server.js
@@ -1,21 +1,46 @@
+import e from "express"
 import express from "express"
+import fs from "fs"
 
 const app = express()
-app.set("views", "./views")
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.set("view engine", "ejs")
 
-// const isLoggedIn = false
+const isLoggedIn = false
 
 app.get("/", (req, res) => {
-  res.render("index", { isLoggedIn: false })
+  res.render("index", { isLoggedIn })
 })
 
 app.get("/login", (req, res) => {
-  res.render("login", { isLoggedIn: false })
+  res.render("login", { isLoggedIn })
+})
+
+app.post("/login", async (req, res) => {
+  const data = JSON.parse(fs.readFileSync("data.json", "utf-8"))
+  console.log(data)
 })
 
 app.get("/signup", (req, res) => {
-  res.render("signup", { isLoggedIn: false })
+  res.render("signup", { isLoggedIn })
+})
+
+app.post("/signup", async (req, res) => {
+  fs.readFile("data.json", "utf-8", (err, data) => {
+    // if (!data) {
+    //   fs.writeFile("data.json", "[]", (err) => {
+    //     console.error(err)
+    //   })
+    // }
+
+    const persons = JSON.parse(data)
+    persons.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(persons))
+
+    res.send("Successfully Regestered!")
+  })
 })
 
 app.listen(process.env.PORT || 3000, () => {
